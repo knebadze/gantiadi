e1d8f9910a1db3159dde7fa2f332be456006e826
knebadze <nebo.nebadze98@gmail.com>
2024-02-02 18:10:09 +0400

    add hamburger component

---
+++ src/components/inc/HamburgerMenu.vue
@@ -0,0 +1,102 @@
+<script setup>
+    import { ref } from 'vue';
+    import { useRoute } from 'vue-router';
+
+    const isRouteActive = (path) => {
+        const route = useRoute();
+        return route.path === path;
+    };
+
+    // hamburger
+    const navbar_nav = ref(false); 
+
+    const toggleNavbarNav = () =>{
+        navbar_nav.value = !navbar_nav.value
+    }
+
+    const navbar_link = ref(false); 
+
+    const toggleNavLink = () =>{
+        navbar_link.value = !navbar_link.value
+    }
+</script>
+<template>
+    <div class="main-responsive-nav">
+        <div class="container">
+            <div class="main-responsive-menu mean-container">
+                <div class="mean-bar">
+                    <a 
+                        href="#" 
+                        class="meanmenu-reveal " 
+                        style="right: 0px; left: auto; text-align: center; text-indent: 0px; font-size: 18px;" 
+                        @click="toggleNavbarNav"
+                    >
+                        <span v-show="!navbar_nav"><span><span></span></span></span>{{ navbar_nav ? 'X' :''  }}
+                    </a>
+                    <nav class="mean-nav" >
+                        <ul class="navbar-nav" v-show="navbar_nav">
+                            <li class="nav-item " @click="event => {navbar_nav = false}">
+                                <router-link to="/" class="nav-link" :class="{'active': isRouteActive('/')}">
+                                    მთავარი
+                                </router-link>
+                            </li>
+                            <li class="nav-item " @click="event => {navbar_nav = false}">
+                                <router-link to="/events" class="nav-link" :class="{'active': isRouteActive('/events')}">
+                                    ოლიმპიადები
+                                </router-link>
+                            </li>
+                            <li class="nav-item " @click="event => {navbar_nav = false}">
+                                <router-link to="/schedule" class="nav-link" :class="{'active': isRouteActive('/schedule')}">
+                                    განრიგი
+                                </router-link>
+                            </li>
+                            <li class="nav-item " @click="event => {navbar_nav = false}">
+                                <router-link to="/rules" class="nav-link" :class="{'active': isRouteActive('/rules')}">
+                                    წესები
+                                </router-link>
+                            </li>
+                            
+
+                            <li class="nav-item" @click="toggleNavLink">
+                                <a href="#" class="nav-link" :class="{'active': isRouteActive('/score')}">
+                                    შედეგები 
+                                    <i class="bx bx-chevron-down"></i>
+                                </a>
+                                <ul class="dropdown-menu" style=" display: block;" v-show="navbar_link">
+                                    <li class="nav-item" @click="event => {navbar_nav = false}">
+                                        <router-link to="/score" class="nav-link" :class="{'active': isRouteActive('/score')}">
+                                            ძებნა
+                                        </router-link>
+                                    </li>
+
+                                    <li class="nav-item" @click="event => {navbar_nav = false}">
+                                        <a href="blog-details.html" class="nav-link">
+                                            რეიტინგი
+                                        </a>
+                                    </li>
+                                </ul>
+                            <a class="mean-expand " href="#" style="font-size: 18px">{{ !navbar_link ?'+' : '-' }}</a></li>
+                            <!-- class="mean-clicked"  - -->
+                            <li class="nav-item " @click="event => {navbar_nav = false}">
+                                    <router-link to="/pay" class="nav-link" :class="{'active': isRouteActive('/pay')}">
+                                    გადახდა
+                                </router-link>
+                            </li>
+                            <li class="nav-item mean-last" @click="event => {navbar_nav = false}">
+                                <a href="contact.html" class="nav-link">
+                                    Contact
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="logo">
+                    <router-link to="/">
+                        <img src="@/assets/img/logo-color-3.png" class="black-logo" alt="image">
+                        <img src="@/assets/img/logo-color-3.png" class="white-logo" alt="image">
+                    </router-link>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- src/components/inc/HeaderInc.vue
+++ src/components/inc/HeaderInc.vue
@@ -1,6 +1,7 @@
 <script setup>
     import { ref, onMounted, onUnmounted } from 'vue';
     import { useRoute } from 'vue-router';
+    import HamburgerMenu from './HamburgerMenu.vue';
 
     // Reactive variable to track whether the navbar is sticky
     const isSticky = ref(false);
@@ -25,18 +26,7 @@
         return route.path === path;
     };
 
-    // hamburger
-    const navbar_nav = ref(false); 
-
-    const toggleNavbarNav = () =>{
-        navbar_nav.value = !navbar_nav.value
-    }
-
-    const navbar_link = ref(false); 
-
-    const toggleNavLink = () =>{
-        navbar_link.value = !navbar_link.value
-    }
+    
 </script>
 <template>
     <div>
@@ -50,107 +40,8 @@
     
    <!-- Start Navbar Area -->
         <div class="navbar-area " :class="{ 'is-sticky': isSticky }">
-            <div class="main-responsive-nav">
-                <div class="container">
-                    <div class="main-responsive-menu mean-container">
-                        <div class="mean-bar">
-                            <a 
-                                href="#" 
-                                class="meanmenu-reveal " 
-                                style="right: 0px; left: auto; text-align: center; text-indent: 0px; font-size: 18px;" 
-                                @click="toggleNavbarNav"
-                            >
-                                <span v-show="!navbar_nav"><span><span></span></span></span>{{ navbar_nav ? 'X' :''  }}
-                            </a>
-                            <nav class="mean-nav" >
-                                <ul class="navbar-nav" v-show="navbar_nav">
-                                    <li class="nav-item " @click="event => {navbar_nav = false}">
-                                        <router-link to="/" class="nav-link" :class="{'active': isRouteActive('/')}">
-                                            მთავარი
-                                        </router-link>
-                                    </li>
-                                    <li class="nav-item " @click="event => {navbar_nav = false}">
-                                        <router-link to="/events" class="nav-link" :class="{'active': isRouteActive('/events')}">
-                                            ოლიმპიადები
-                                        </router-link>
-                                    </li>
-                                    <li class="nav-item " @click="event => {navbar_nav = false}">
-                                        <router-link to="/schedule" class="nav-link" :class="{'active': isRouteActive('/schedule')}">
-                                            განრიგი
-                                        </router-link>
-                                    </li>
-                                    <li class="nav-item " @click="event => {navbar_nav = false}">
-                                        <router-link to="/rules" class="nav-link" :class="{'active': isRouteActive('/rules')}">
-                                            წესები
-                                        </router-link>
-                                    </li>
-                                    
-
-                                    <li class="nav-item" @click="toggleNavLink">
-                                        <a href="#" class="nav-link" :class="{'active': isRouteActive('/score')}">
-                                            შედეგები 
-                                            <i class="bx bx-chevron-down"></i>
-                                        </a>
-                                        <ul class="dropdown-menu" style=" display: block;" v-show="navbar_link">
-                                            <li class="nav-item" @click="event => {navbar_nav = false}">
-                                                <router-link to="/score" class="nav-link" :class="{'active': isRouteActive('/score')}">
-                                                    ძებნა
-                                                </router-link>
-                                            </li>
-
-                                            <li class="nav-item" @click="event => {navbar_nav = false}">
-                                                <a href="blog-details.html" class="nav-link">
-                                                    რეიტინგი
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    <a class="mean-expand " href="#" style="font-size: 18px">{{ !navbar_link ?'+' : '-' }}</a></li>
-                                    <!-- class="mean-clicked"  - -->
-                                    <li class="nav-item " @click="event => {navbar_nav = false}">
-                                            <router-link to="/pay" class="nav-link" :class="{'active': isRouteActive('/pay')}">
-                                            გადახდა
-                                        </router-link>
-                                    </li>
-                                    <li class="nav-item mean-last" @click="event => {navbar_nav = false}">
-                                        <a href="contact.html" class="nav-link">
-                                            Contact
-                                        </a>
-                                    </li>
-                                </ul>
-                            </nav>
-                        </div>
-                        <div class="logo">
-                            <router-link to="/">
-                                <img src="@/assets/img/logo-color-3.png" class="black-logo" alt="image">
-                                <img src="@/assets/img/logo-color-3.png" class="white-logo" alt="image">
-                            </router-link>
-                        </div>
-                    </div>
-                    <!-- <div class="main-responsive-menu d-flex justify-content-between">
-                        
-                        <div class="logo">
-                            <router-link to="/">
-                                <img src="@/assets/img/logo-color-3.png" class="black-logo" alt="image">
-                                <img src="@/assets/img/logo-color-3.png" class="white-logo" alt="image">
-                            </router-link>
-                        </div>
-                     
-                        <div class="mean-bar dropdown">
-                            <a  class="meanmenu-reveal dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                                <i class="bx bx-menu"></i>
-                            </a>
-                                <ul class="dropdown-menu" >
-                                    <li class=" d-flex justify-content-between">
-                                        <a href="#" class="dropdown-item">
-                                            Home 
-                                        </a>
-                                    </li>
-                                </ul>
-                        </div>
-                    </div> -->
-                </div>
-            </div>
-
+            
+            <HamburgerMenu />
             <div class="main-navbar " >
                 <div class="container">
                     <nav class="navbar navbar-expand-md navbar-light" >
